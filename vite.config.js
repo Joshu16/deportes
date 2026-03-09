e609f3b20ba6f63dcef80d083841daac28d896b2
Joshua <jsaborio1604@gmail.com>
2026-03-09 13:36:16 -0600

    Pagina lista3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // base es necesario para GitHub Pages (reemplaza con el nombre de tu repo)
 export default defineConfig({
-  base: '/empresa-ventas/',
+  base: '/deportes/',
   plugins: [react()],
 })
